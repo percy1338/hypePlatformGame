fdbb0462f7a60b5dd959265ec6617757088f2fe9
Mathieu te Stroete <Mathieutestroete@Hotmail.com>
2018-05-14 15:41:42 +0200

    *clap clap* MEEP REVIEW

--- 3dPlatformere/Assets/script/CameraLook.cs
+++ 3dPlatformere/Assets/script/CameraLook.cs
@@ -16,9 +16,9 @@ public class CameraLook : MonoBehaviour {
 	void Start () {
         Player = this.transform.parent.gameObject; //Gets the player, which is the parent of the camera (since the camera is nested in there)
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
 
         var mouseDirection = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")); //Gets the raw input from the mouse and puts this in a vec2 consisting of x and y.
 
@@ -29,16 +29,32 @@ public class CameraLook : MonoBehaviour {
 
         mouselook.y = Mathf.Clamp(mouselook.y, -90f, 90f); //Makes it so that you can't rotate more then 90 degrees on the y-axis, so you can't look flip over
 
-      // if (!Player.GetComponent<backupPlayer>().UnlockCamera)
-      // {
+        if (!Player.GetComponent<LittlePlayer>().UnlockCamera)
+        {
             transform.localRotation = Quaternion.AngleAxis(-mouselook.y, Vector3.right); //Rotates the camera.
             Player.transform.localRotation = Quaternion.AngleAxis(mouselook.x, Player.transform.up); //Rotates the player.
-      // }
-      // else
-      // {
-        //   transform.localRotation = Quaternion.AngleAxis(-mouselook.y, Vector3.right); //Rotates the camera.
-        //   transform.localRotation = Quaternion.AngleAxis(mouselook.x, transform.up); //Rotates the camera.
-        //    Player.transform.localRotation = Player.GetComponent<backupPlayer>().currentWall.rotation;
+        }
+        else
+        {
+            transform.localRotation = Quaternion.AngleAxis(-mouselook.y, Vector3.right); //Rotates the camera.
+            //Player.transform.localRotation = Player.GetComponent<LittlePlayer>().currentWall.rotation; // sets player to rotation of the wall
+            //transform.rotation = Quaternion.FromToRotation(Vector3.right, hit.normal);
+        }
+
+
+
+       // if (!Player.GetComponent<LittlePlayer>().UnlockCamera) // when not wallrunning
+       //{
+       //     transform.localRotation = Quaternion.AngleAxis(-mouselook.y, Vector3.right); //Rotates the camera.
+       //     Player.transform.localRotation = Quaternion.AngleAxis(mouselook.x, Player.transform.up); //Rotates the player.
+       //}
+       //else //When wallrunning
+       //{
+       //    transform.localRotation = Quaternion.AngleAxis(-mouselook.y, Vector3.right); //Rotates the camera.
+       //     transform.localRotation = Quaternion.AngleAxis(mouselook.x, Vector3.up);
+       //    transform.localRotation = Quaternion.AngleAxis(mouselook.x, transform.up); // rotates some other part of the camera
+
+       //     Player.transform.localRotation = Player.GetComponent<LittlePlayer>().currentWall.rotation; // sets player to rotation of the wall!
        //}
     }
 }
--- 3dPlatformere/Assets/script/LittlePlayer.cs
+++ 3dPlatformere/Assets/script/LittlePlayer.cs
@@ -4,124 +4,272 @@ using UnityEngine;
 
 public class LittlePlayer : MonoBehaviour
 {
+    [Header("basic player Properties")]
     public float speed = 10.0f;
-    public float MaxSpeed = 20;
-    public float JumpForce = 0f;
 
-    private bool _wallRun;
+    [Header("Jump Properties")]
+    public float JumpForce = 0.0f;
+    private bool _grounded = false;
 
-    private bool _grounded;
-    private Rigidbody rb;
-    private CapsuleCollider cap;
-    Vector3 movement;
-    private Vector3 _resetGravity;
+    [Header("Dash Properties")]
+    public float DashForce = 0.0f;
+    private int _dashCount = 0;
+
+    [Header("Slide Properties")]
+    public float SlideForce;
+    public float MaxSlideDistance = 120;
+    private Vector3 _slideVec;
+    private float _curSlideDistance;
+    private bool Slideing;
+
+    [Header("WallRun Properties")]
+    public float _wallRunForce = 0.0f;
+    private Vector3 _wallRunVec;
+    private bool _wallRun = false;
     private Transform LastWall;
+    public Transform currentWall;
+
+    public bool UnlockCamera = false;
+
+
+    private bool leftSide;
+    private bool rightSide;
+    // Raycasts.
+    public RaycastHit Hitleft;
+    public RaycastHit HitRight;
+
+    private Rigidbody _rb;
+    private Vector3 _movement;
+    private CapsuleCollider _cap;
+    private Vector3 _test;
+    
 
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
-        cap = gameObject.GetComponent<CapsuleCollider>();
+        _rb = gameObject.GetComponent<Rigidbody>();
+        _cap = gameObject.GetComponent<CapsuleCollider>();
     }
 
     void Update()
     {
-        movement.x = 0;
-        movement.z = 0;
+        GroundedCheck();
 
+        Movement();
+        Jumping();
+        Wallrunning();
+        Sliding();
 
-        if (CanMove(transform.forward * Input.GetAxis("Vertical")))
-        {
-            movement.z = Input.GetAxis("Vertical");
-        }
+        
 
-        if (CanMove(transform.right * Input.GetAxis("Horizontal")))
-        {
-            movement.x = Input.GetAxis("Horizontal");
-        }
-
-        movement = transform.rotation * movement;
-
-
-        if (rb.velocity.magnitude > MaxSpeed)
-        {
-            rb.velocity = rb.velocity.normalized * MaxSpeed;
-        }
-
-
-
-        RaycastHit hit;
-        Ray jumpRay = new Ray(this.transform.position, Vector3.down);
-        if (Physics.Raycast(jumpRay, out hit, 1.0f))
-        {
-            if (hit.transform.tag == "Wall")
-            {
-                _grounded = true;
-                LastWall = null;
-            }
-
-        }
-
-        if ((Input.GetKeyDown(KeyCode.Space)) && _grounded)
-        {
-            rb.AddForce(Vector3.up * JumpForce);
-            _grounded = false;
-        }
-
-        Debug.Log(_wallRun);
-
+        _movement = transform.rotation * _movement;
     }
 
     void FixedUpdate()
     {
-        Vector3 gravFix = new Vector3(0, rb.velocity.y, 0);
-        rb.velocity = movement * speed;
-        rb.velocity += gravFix;
+        Vector3 gravFix = new Vector3(0, _rb.velocity.y, 0);
+
+        _rb.velocity = _movement * speed;
+        _rb.velocity += gravFix;
+    }
+
+    private void GroundedCheck()
+    {
+        RaycastHit hit;
+        Ray jumpRay = new Ray(this.transform.position, Vector3.down);
+
+
+        if (Physics.Raycast(jumpRay, out hit, 1.01f))
+        {
+            if (hit.transform.tag == "Ground" || hit.transform.tag == "Wall")
+            {
+                _grounded = true;
+            }
+
+        }
+        else
+        {
+            _grounded = false;
+        }
+    }
+
+    private void Movement()
+    {
+        _test = Vector3.zero;
+
+        _movement = _test;
+
+        if (!Slideing)
+        {
+            if (WallRunCheck(transform.forward * Input.GetAxis("Vertical")))
+            {
+                _test.z = Input.GetAxis("Vertical");
+            }
+
+            if (WallRunCheck(transform.right * Input.GetAxis("Horizontal")))
+            {
+                _test.x = Input.GetAxis("Horizontal");
+            }
+        }
+        _test.Normalize();
+        _movement += _test;
+
+    }
+
+    private void Jumping()
+    {
+        if ((Input.GetKeyDown(KeyCode.Space)) && _grounded == true)
+        {
+            _grounded = false;
+            _rb.AddForce(Vector3.up * JumpForce);
+        }
+    }
+
+    private void Wallrunning()
+    {
+        if (_wallRun)
+        {
+          //UnlockCamera = true;
+        }
+        else
+        {
+            UnlockCamera = false;
+        }
+    }
+
+    private void Sliding()
+    {
+        if (_wallRun != true)
+        {
+            if ((Input.GetKeyDown(KeyCode.LeftShift)))
+            {
+                _slideVec = transform.forward;
+                Slideing = true;
+            }
+            if ((Input.GetKeyUp(KeyCode.LeftShift)))
+            {
+                Slideing = false;
+            }
+            if (Slideing)
+            {
+                _cap.height = 0.5f;
+                _rb.AddForce(_slideVec * SlideForce);
+            }
+            else
+            {
+                _cap.height = 2;
+            }
+            if (_curSlideDistance >= MaxSlideDistance)
+            {
+                Slideing = false;
+                _curSlideDistance = 0;
+            }
+        }
     }
 
 
-
-
-
-
-
-
-
-    bool CanMove(Vector3 direction)
+    private bool WallRunCheck(Vector3 direction)
     {
-        float distanceToPoints = cap.height / 2 - cap.radius;
+        float distanceToPoints = _cap.height / 2 - _cap.radius;
 
-        Vector3 point1 = transform.position + cap.center + Vector3.up * distanceToPoints;
-        Vector3 point2 = transform.position + cap.center - Vector3.up * distanceToPoints;
-        float radius = cap.radius * 0.95f;
+        Vector3 point1 = transform.position + _cap.center + Vector3.up * distanceToPoints;
+        Vector3 point2 = transform.position + _cap.center - Vector3.up * distanceToPoints;
+        float radius = _cap.radius * 0.95f;
         float castDistance = 0.5f;
 
         RaycastHit[] hits = Physics.CapsuleCastAll(point1, point2, radius, direction, castDistance);
 
         foreach (RaycastHit objectHit in hits)
         {
-            if (objectHit.transform.tag == "Wall" && _grounded == false)
+            if (objectHit.transform.tag == "Wall" || objectHit.transform.tag == "Ground")
             {
                 _wallRun = true;
+                currentWall = objectHit.transform;
+
+                checkSide();
+                wallrunCamera(objectHit);
+
                 if ((Input.GetKeyDown(KeyCode.Space) && LastWall != objectHit.transform))
                 {
                     LastWall = objectHit.transform;
-
+                    _wallRun = false;
                     WallJump();
                 }
                 return false;
             }
         }
         _wallRun = false;
+
         return true;
     }
 
+    private void checkSide()
+    {
+        RaycastHit left;
+        RaycastHit right;
 
+
+        Ray LeftRay = new Ray(this.transform.position, transform.right);
+        Ray RightRay = new Ray(this.transform.position, -transform.right);
+
+        if (Physics.Raycast(LeftRay, out left, 1.01f))
+        {
+            if (left.transform.tag == "Ground" || left.transform.tag == "Wall")
+            {
+                rightSide = false;
+                leftSide = true;
+            }
+        }
+
+
+        if (Physics.Raycast(RightRay, out right, 1.01f))
+        {
+            if (right.transform.tag == "Ground" || right.transform.tag == "Wall")
+            {
+                rightSide = true;
+                leftSide = false;
+            }
+        }
+    }
+
+    private void wallrunCamera(RaycastHit objectHit)
+    {
+        if (rightSide == true)
+        {
+            transform.rotation = Quaternion.FromToRotation(Vector3.right, objectHit.normal);
+        }
+
+        if (leftSide == true)
+        {
+            transform.rotation = Quaternion.FromToRotation(-Vector3.right, objectHit.normal);
+        }
+
+
+    }
 
     private void WallJump() //Jumping from a wall. Different then a normal jump!
     {
-        Vector3 test = new Vector3(rb.velocity.x,0,rb.velocity.z);
-        rb.velocity = test;
-        _grounded = false;
-        rb.AddForce(Vector3.up * JumpForce);
+        Vector3 test = new Vector3(_rb.velocity.x, 0, _rb.velocity.z);
+        _rb.velocity = test;
+        _rb.AddForce(Vector3.up * JumpForce);
     }
+
+    private void Airdash()
+    {
+        _rb.AddForce(Vector3.forward * DashForce);
+        _dashCount++;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (other.name == "CrawlSpace")
+        //    {
+        //       Slideing = true;
+        //   }
+        //  else
+        //  {
+        //       Slideing = false;
+        //  }
+        //  Debug.Log("crawling");
+    }
+
 }
